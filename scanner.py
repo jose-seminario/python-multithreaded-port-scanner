df66de64d4ea143874a6d3d5fa14392bc21aabd5
Jose Seminario <chuquipoma123@gmail.com>
2026-07-29 22:34:25 +0000

    Set up thread pool for concurrent scanning

--- scanner.py
+++ scanner.py
@@ -1,4 +1,5 @@
 import socket
+from concurrent.futures import ThreadPoolExecutor
 
 
 def scanner(ip, port):
@@ -21,3 +22,12 @@ def get_service(port):
         return socket.getservbyport(port)
     except:
         return "Unknown"
+
+def scan_port_range(ip, start_port, end_port):
+    open_ports = []
+    
+    with ThreadPoolExecutor() as executor:
+        futures = []
+
+        for port in range(start_port, end_port + 1):
+            future = executor.submit(scanner, ip, port)
